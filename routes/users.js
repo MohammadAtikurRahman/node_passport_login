b9ff8fc8d4316ed077baaff407f9525970efde09
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-21 11:26:56 +0600

    login & Register page created

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 //login page
-router.get('/login', (req, res) => res.send('login'));
+router.get('/login', (req, res) => res.render('login'));
 
 //register page
-router.get('/register', (req, res) => res.send('register'));
+router.get('/register', (req, res) => res.render('register'));
  
 
 
