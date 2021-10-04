9d2cd6163ee2e809ef10963d0ad6dca2b96ad72d
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-10-04 19:58:11 +0600

    ok

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,56 @@ router.get('/login', (req, res) => res.render('login'));
 router.get('/register', (req, res) => res.render('register'));
  
 
+//register handle
+router.post('/register', (req, res) => {
+  
+   const { name,email,password,password2 } = req.body;
+
+   let errors =[];
+   //check required fields
+   if(!name || !email || !password || !password2) {
+
+      errors.push({ msg: 'Please fill in the all fields'});
+
+   }
+
+   //checkpassword match
+   if(password !== password2){
+
+    errors.push({ msg: 'Password do not match'});
+
+   }
+
+
+   // check password should 6 character long
+
+    if(password.lenght <6 ){
+
+       errors.push({msg: 'Password should be at least 6 characters long'});
+
+    }
+
+    if(errors.length > 0){
+            
+        res.render('register',{
+
+           errors, 
+           name,
+           email,
+           password,
+           password2 
+        })
+
+    } else{
+
+
+        res.send('pass')
+    }
+
+
+
+})
+
 
 
 module.exports = router;
