4ffec05575ebe8239540562fc08f8c94714ef78a
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-10-06 01:32:49 +0600

    ended

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-router.get('/',(req,res) => res.render('welcome'));
+//welcome page
+router.get('/', forwardAuthenticated,(req,res) => res.render('welcome'));
 
+//dashboard
+
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+ res.render('dashboard',{
+
+    name: req.user.name
+ }));
 
 
 module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+
+// // Welcome Page
+// router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// // Dashboard
+// router.get('/dashboard', ensureAuthenticated, (req, res) =>
+//     res.render('dashboard', {
+//         user: req.user
+//     })
+// );
+
+// module.exports = router;
